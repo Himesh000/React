e025e7445de8c8e7499e0b020131ae482ebf8d8e
Himesh Verma <vermahimesh001@gmail.com>
2025-06-14 18:00:10 +0530

    feat: implement custom render function and basic HTML structure for custom React app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,18 @@ import Test from "./Test.jsx";
 import "./App.css";
 
 function App() {
+  const username = "Himesh Verma";
+
   return (
     <>
       <Test />
-      <h1>Himesh Verma</h1>
+      <h1>{username}</h1> 
     </>
   );
 }
 
 export default App;
+
+
+//Notes:-
+//In React, an "evaluated expression" is any valid JavaScript expression inside {} in JSX that returns a value for rendering.
