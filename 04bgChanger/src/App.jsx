7f945402e8125ea5e1af627e66554c8d80f62d25
Himesh Verma <vermahimesh001@gmail.com>
2025-06-15 15:03:15 +0530

    feat: initialize 04bgChanger project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Created Vite configuration for React and Tailwind CSS integration.
    - Implemented main application structure in App.jsx with color-changing functionality.
    - Added styles in App.css and index.css for layout and design.
    - Included SVG assets for React and Vite logos.
    - Set up initial project structure with necessary files and folders.

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("Olive");
+
+  return (
+    <div
+      className="w-screen h-screen duration-200"
+      style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-black px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("#606c38")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "#606c38" }}>
+            Olive Green
+          </button>
+          <button
+            onClick={() => setColor("#283618")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "#283618" }}>
+            Deep Forest Green
+          </button>
+          <button
+            onClick={() => setColor("#fefae0")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "#fefae0" }}>
+            Ivory
+          </button>
+          <button
+            onClick={() => setColor("#dda15e")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "#dda15e" }}>
+            Goldenrod
+          </button>
+          <button
+            onClick={() => setColor("#bc6c25")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "#bc6c25" }}>
+            Rust
+          </button>  
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
